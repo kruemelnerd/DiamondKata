5c6af4a61e6fe14bc08ef098602980274e711882
Philipp Veit <philipp.veit@devk.de>
2023-06-14 13:48:49 +0200

    init & first test

--- src/main/java/de/kruemelnerd/diamondkata/DiamondGenerator.java
+++ src/main/java/de/kruemelnerd/diamondkata/DiamondGenerator.java
@@ -1,2 +1,10 @@
-package de.kruemelnerd.diamondkata;public class DiamondGenerator {
+package de.kruemelnerd.diamondkata;
+
+public class DiamondGenerator {
+
+    public char[][] generateDiamond() {
+        char[][] diamondMap = new char[1][1];
+        diamondMap[0][0] = 'A';
+        return diamondMap;
+    }
 }
--- src/test/java/de/kruemelnerd/diamondkata/DiamondGeneratorTest.java
+++ src/test/java/de/kruemelnerd/diamondkata/DiamondGeneratorTest.java
@@ -2,11 +2,15 @@ package de.kruemelnerd.diamondkata;
 
 import org.junit.jupiter.api.Test;
 
-public class ProcessorTest {
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+public class DiamondGeneratorTest {
 
     @Test
     void generate_letter_a(){
-        
+        DiamondGenerator generator = new DiamondGenerator();
+        char[][] diamondMap = generator.generateDiamond();
+        assertThat(diamondMap[0][0], is('A'));
     }
 
 }
