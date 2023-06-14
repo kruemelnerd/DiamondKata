c5d5f7fdc571385795a67aae37c2add3481f2ae7
Philipp Veit <philipp.veit@devk.de>
2023-06-14 14:53:12 +0200

    Write Letter B

--- src/main/java/de/kruemelnerd/diamondkata/DiamondGenerator.java
+++ src/main/java/de/kruemelnerd/diamondkata/DiamondGenerator.java
@@ -2,9 +2,55 @@ package de.kruemelnerd.diamondkata;
 
 public class DiamondGenerator {
 
-    public char[][] generateDiamond() {
-        char[][] diamondMap = new char[1][1];
-        diamondMap[0][0] = 'A';
+    //String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    String alphabet = "AB";
+    final public char BLANK = ' ';
+
+    public char[][] generateDiamond(char goalLetter) {
+        int positionGoalLetter = alphabet.indexOf(goalLetter) + 1;
+        int widthDiamondMap = positionGoalLetter * 2 - 1;
+        int heightDiamondMap = positionGoalLetter;
+
+        char[][] diamondMap = new char[heightDiamondMap][widthDiamondMap];
+
+
+        makeEverythingBlank(heightDiamondMap, widthDiamondMap, diamondMap);
+
+
+        for (int height = 0; height < heightDiamondMap; height++) {
+            char letterFromHeight = alphabet.charAt(height);
+            int positionOfLetter = widthDiamondMap - (heightDiamondMap + height);
+            if(heightDiamondMap == 1){
+                positionOfLetter = 0;
+            }
+            diamondMap[height][positionOfLetter] = letterFromHeight;
+            diamondMap[height][(widthDiamondMap - 1) - positionOfLetter] = letterFromHeight;
+            // Breite: 3
+            // Höhe: 1
+            // Länge: 2
+            // gewünschte Position: 2
+            // =3 / 2 = 1,5
+            // = 5 / 2 = 2,5
+
+
+            //for (int width = 0; width < widthDiamondMap; width++) {
+            //    diamondMap[height][width] = letterFromHeight;
+            //}
+        }
+
+        //diamondMap[heightDiamondMap - 1][widthDiamondMap - 1] = goalLetter;
         return diamondMap;
     }
+
+    private void makeEverythingBlank(int heightDiamondMap, int widthDiamondMap, char[][] diamondMap) {
+        for (int height = 0; height < heightDiamondMap; height++) {
+            for (int width = 0; width < widthDiamondMap; width++) {
+                diamondMap[height][width] = BLANK;
+            }
+        }
+    }
 }
+
+
+// Jede Linie
+// 1 Linie
--- src/test/java/de/kruemelnerd/diamondkata/DiamondGeneratorTest.java
+++ src/test/java/de/kruemelnerd/diamondkata/DiamondGeneratorTest.java
@@ -6,11 +6,47 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 public class DiamondGeneratorTest {
 
+    // A
     @Test
     void generate_letter_a(){
         DiamondGenerator generator = new DiamondGenerator();
-        char[][] diamondMap = generator.generateDiamond();
+        char[][] diamondMap = generator.generateDiamond('A');
         assertThat(diamondMap[0][0], is('A'));
     }
+
+    // _A_ 3-2 = 1 =  Breite - Höhe + 1
+    // B_B 3-3 = 0
+    // _A_ 3-2 = 1
+    @Test
+    void generate_letter_b(){
+        DiamondGenerator generator = new DiamondGenerator();
+        char[][] diamondMap = generator.generateDiamond('B');
+
+        assertThat(diamondMap[0][0], is(' '));
+        assertThat(diamondMap[0][1], is('A'));
+        assertThat(diamondMap[0][2], is(' '));
+
+        assertThat(diamondMap[1][0], is('B'));
+        assertThat(diamondMap[1][1], is(' '));
+        assertThat(diamondMap[1][2], is('B'));
+    }
+
+    // 01234 = 2*Position-1
+    // __A__ 5-3 = 2
+    // _B_B_ 5-4 = 1
+    // C___C 5-5 = 0
+    // _B_B_ 5-4 = 1
+    // __A__ 5-3 = 0
+    //
+
+    //   0123456 = 2*Position-1 = 7
+    // 0 ___A___ 7-4 = 3 = Breite - Höhe  + 0
+    // 1 __B_B__ 7-5 = 2 = Breite - Höhe  + 1
+    // 2 _C___C_ 7-6 = 1 = Breite - Höhe  + 2
+    // 3 D_____D 7-7 = 0 = Breite - Höhe  + 3
+
+    // 4 _C___C_
+    // 5 __B_B__
+    // 6 ___A___
 
 }
